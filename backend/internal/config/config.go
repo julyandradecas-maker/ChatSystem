09c88a4b460fbc55c8caf27c461223fb0b8dadc7
julyandradecas-maker <july.andrade.cas@gmail.com>
2026-05-20 11:18:15 -0500

    Update config.go

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,1 +1,29 @@
+package config
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	RedisURL  string
+	JWTSecret string
+	APIURL    string
+	Port      string
+}
+
+func Load() *Config {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Println("No se encontró .env, usando variables del sistema")
+	}
+
+	return &Config{
+		RedisURL:  os.Getenv("REDIS_URL"),
+		JWTSecret: os.Getenv("JWT_SECRET"),
+		APIURL:    os.Getenv("API_URL"),
+		Port:      os.Getenv("PORT"),
+	}
+}
