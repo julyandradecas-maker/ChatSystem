beeb7ca5bd3935b7c0438fa62d7156eb7f586ff7
julyandradecas-maker <july.andrade.cas@gmail.com>
2026-05-20 11:18:35 -0500

    Update models.go

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -1,1 +1,23 @@
-..
+package models
+
+import "time"
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+}
+
+type Message struct {
+	ID            string    `json:"id"`
+	RoomID        string    `json:"room_id"`
+	UserID        string    `json:"user_id"`
+	Username      string    `json:"username"`
+	Texto         string    `json:"texto"`
+	FechaCreacion time.Time `json:"fechaCreacion"`
+}
+
+type Room struct {
+	ID            string    `json:"id"`
+	Nombre        string    `json:"nombre"`
+	FechaCreacion time.Time `json:"fechaCreacion"`
+}
